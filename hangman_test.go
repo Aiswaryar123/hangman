a9548c6a74e7fbb1b055f5dcc073978a9eecc6ac
Aiswarya r <aiswaryarathee123@gmail.com>
2025-10-15 12:14:55 +0530

    RED:add test for capital  words

---
+++ hangman_test.go
@@ -0,0 +1,18 @@
+package main
+
+
+import (
+	"strings"
+	"testing"
+)
+
+
+func TestSecretWordNoCapitals(t *testing.T) {
+	wordList := "/usr/share/dict/words"
+	secretWord := getSecretWord(wordList)
+	if secretWord != strings.ToLower(secretWord) {
+		t.Errorf("Should not get words with capital letters. Got %s", secretWord)
+	}
+
+
+}
